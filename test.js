77faf779b43174e684d03dc4b310edc0cb6ace65
arturcordeiromie <artur.cordeiro@omie.com.br>
2024-09-04 09:53:00 -0300

    first commit

---
+++ test.js
@@ -0,0 +1,7 @@
+function printHelloWorld() {
+    for (let i = 0; i < 5; i++) {
+        console.log("Hello, World!");
+    }
+}
+
+printHelloWorld();
---
+++ test.py
@@ -0,0 +1,5 @@
+def hello_world_loop():
+    for _ in range(5):
+        print("Hello, World!")
+
+hello_world_loop()
